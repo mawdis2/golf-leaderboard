550ba2fd13f54017184239d88caeeb2d537807b5
Martin Awdisho <mawdisho@gmail.com>
2025-03-10 18:46:39 -0500

    Add user loader to extensions

--- extensions.py
+++ extensions.py
@@ -6,6 +6,11 @@ db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 
+@login_manager.user_loader
+def load_user(user_id):
+    from models import User
+    return User.query.get(int(user_id))
+
 def init_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
