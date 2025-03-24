60a9055abbc31bc5b4b5186eb9fdf10fc4f801a1
Martin Awdisho <mawdisho@gmail.com>
2025-03-23 19:57:52 -0500

    Add CSRF protection for tournament match forms

--- extensions.py
+++ extensions.py
@@ -1,10 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
+csrf = CSRFProtect()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -15,6 +17,7 @@ def init_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
+    csrf.init_app(app)
     
     login_manager.login_view = 'main.login'
     login_manager.login_message = 'Please log in to access this page.'
